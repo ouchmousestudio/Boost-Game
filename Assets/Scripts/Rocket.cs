d8cc1ff8c350553a00040c967645b04b1359f579
Miles <mairuzu@gmail.com>
2019-08-07 13:14:36 +0900

    Very basic level design - start

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,35 +5,79 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-
+    //Components
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    //Serialized Fields
+    [SerializeField] float rcsThrust;
+    [SerializeField] float mainThrust;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        processInput();
+        Thrust();
+        Rotate();
     }
 
-    private void processInput()
+    private void Rotate()
     {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        //manual control
+        rigidBody.freezeRotation = true;
+
+        float frameDepRotation = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            print("left pressed");
+            transform.Rotate(Vector3.forward * frameDepRotation);
         }
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            print("right pressed");
+            transform.Rotate(-Vector3.forward * frameDepRotation);
+        }
+        //stop
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Safe!"); //todo remove
+                //Landing Pad
+                break;
+            case "Refuel":
+                print("Refuelling"); //todo remove
+                //Refuel ship
+                break;
+            default:
+                print("Dead!"); //todo remove
+                // Kill P;ayer
+                break;
         }
     }
 }
