ed110669a403d82bff65d61e61b5ff9b8257dd35
Miles <mairuzu@gmail.com>
2019-08-08 10:48:18 +0900

    Added Sound Effects

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -12,6 +13,13 @@ public class Rocket : MonoBehaviour
     //Serialized Fields
     [SerializeField] float rcsThrust;
     [SerializeField] float mainThrust;
+    [Header("Rocket Audio")]
+    [SerializeField] AudioClip mainEngineSFX;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] AudioClip levelCompleteSFX;
+
+    enum State { Alive, Dying, Succeed }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +31,12 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            //Respond to key inputs
+            Thrust();
+            Rotate();
+        }
     }
 
     private void Rotate()
@@ -50,11 +62,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
@@ -62,9 +70,21 @@ public class Rocket : MonoBehaviour
         }
     }
 
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngineSFX);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        switch(collision.gameObject.tag)
+        //Only allow one collision
+        if (state != State.Alive) { return; }
+
+        switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("Safe!"); //todo remove
@@ -74,10 +94,28 @@ public class Rocket : MonoBehaviour
                 print("Refuelling"); //todo remove
                 //Refuel ship
                 break;
+            case "Goal":
+                state = State.Succeed;
+                audioSource.PlayOneShot(levelCompleteSFX);
+                Invoke("LoadNextScene", 1f);
+                break;
             default:
-                print("Dead!"); //todo remove
-                // Kill P;ayer
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
+                audioSource.Stop();
+                audioSource.PlayOneShot(deathSFX);
+                // Kill Player
                 break;
         }
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1); //todo add more levels
     }
 }
